88d1b9a4f4a4629811d560ebe3b62fdea86ece89
KZL <lz3450@outlook.com>
2023-08-28 11:15:26 -0500

    script to start qemu vm

--- qemu/start.sh
+++ qemu/start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-taskset -c 8-15 qemu-system-x86_64 \
-    -smp 8 -m 16G -enable-kvm \
+taskset -c 0-15 qemu-system-x86_64 \
+    -smp 16 -m 16G -enable-kvm \
     -cpu host,+sgx,+sgxlc,+sgx-provisionkey,+sgx-debug \
-    -object memory-backend-epc,id=mem1,size=64M,prealloc=on \
+    -object memory-backend-epc,id=mem1,size=65M,prealloc=on \
     -M sgx-epc.0.memdev=mem1 \
     -bios /usr/share/ovmf/OVMF.fd \
     -drive file=./ubuntu2004.img,index=0,media=disk,format=raw \

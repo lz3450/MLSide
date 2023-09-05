13bf68d94f6846cbd15b19272eab19c32aa18eaa
KZL <lz3450@outlook.com>
2023-09-05 13:45:59 -0500

    qemu

--- qemu/chroot.sh
+++ qemu/chroot.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/bash
 
+mountpoint=ubuntu2004
+loopdev=$(sudo losetup -fP --show ubuntu2004.img)
+
+cleanup() {
+    set +e
+
+    if [ -n "${mountpoint}" ]; then
+        for attempt in $(seq 10); do
+            for fs in dev/pts dev sys proc run; do
+                mount | grep -q "${mountpoint}/${fs}" && sudo umount -R "${mountpoint}/${fs}" 2> /dev/null
+            done
+            mount | grep -q "${mountpoint}" && sudo umount -R "${mountpoint}" 2> /dev/null
+            if [ $? -ne 0 ]; then
+                break
+            fi
+            sleep 1
+        done
+    fi
+
+    sudo losetup -d "${loopdev}"
+    if [ -d "${mountpoint}" ]; then
+        rmdir "${mountpoint}"
+    fi
+}
+trap cleanup EXIT
+
 set -e
 # set -x
 
-mountpoint=ubuntu2004
 mkdir -p "$mountpoint"
-
-loopdev=$(sudo losetup -fP --show ubuntu2004.img)
 sudo mount ${loopdev}p2 "$mountpoint"
 
 # Check if the mount was successful
@@ -19,13 +42,16 @@ else
     exit 1
 fi
 sudo mount ${loopdev}p1 "$mountpoint"/boot/efi
-
-# 
-sudo mkdir -p "$mountpoint"/run/systemd/resolve
-sudo cp /etc/resolv.conf "$mountpoint"/run/systemd/resolve/stub-resolv.conf
+for fs in dev sys proc run; do
+    sudo mount --rbind /"${fs}" "${mountpoint}"/"${fs}"
+    sudo mount --make-rslave "${mountpoint}"/"${fs}"
+done
 
 sudo chroot "$mountpoint"/ /bin/zsh
 
+for fs in dev sys proc run; do
+    sudo umount -R "${mountpoint}"/$fs
+done
 sleep 1
 sudo umount -R "$mountpoint"
 sleep 1
